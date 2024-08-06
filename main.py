17b88383b06279a0b5321586c5c37bf462b38a4c
Aman kumar <113019735+popsi-techy@users.noreply.github.com>
2024-08-06 14:46:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# This entrypoint file to be used in development. Start by reading README.md
+# Entrypoint file
 import time_series_visualizer
 from unittest import main
 
-# Test your function by calling it here
+# Test function
 time_series_visualizer.draw_line_plot()
 time_series_visualizer.draw_bar_plot()
 time_series_visualizer.draw_box_plot()
